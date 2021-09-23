e8e48f908abc3c7a9a34aa9ea878c124e0931277
johnsonzead@gmail.com <johnsonzead@gmail.com>
2021-09-24 04:01:35 +1000

    graphql & applo express basic setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { Server } from "http";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, {
-    title: "1st post",
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
-  await orm.em.persistAndFlush(post);
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server is listening at port 4000 ");
+  });
 };
 
 main().catch((err) => {
