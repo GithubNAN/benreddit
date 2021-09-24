c96ef66aaca89c91663372dfeaf090b87cd1b05a
NNN <johnsonzead@gmail.com>
2021-09-25 09:15:59 +1000

    Basic cookie & session setup

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,14 @@
 import { IDatabaseDriver, Connection, EntityManager } from "@mikro-orm/core";
+import { Response, Request } from "express";
+
+declare module "express-session" {
+  interface SessionData {
+    userId: number;
+  }
+}
 
 export type MyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
+  res: Response;
+  req: Request;
 };
